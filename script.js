48276f8007e9dc7a45262bf1fd81f35e4937a4bc
Tulio <221194504+tulin404@users.noreply.github.com>
2025-11-03 19:17:40 -0300

    Func display completed

--- script.js
+++ script.js
@@ -3,13 +3,32 @@
 const bigNumArea = document.getElementById('big-num-area');
 const smallNumArea = document.getElementById('small-num-area');
 const numBtns = document.getElementsByClassName('num-btn');
+const operatorBtns = document.getElementsByClassName('operator-btn');
 const clearBtn = document.getElementById('clear');
 
 // CREATING THE BASIC FUNCTIONS
 function clear() {
     bigNumArea.innerText = 0;
     smallNumArea.innerText = null;
-}
+};
 
+function display(event) {
+    const numArr = bigNumArea.innerText.split('');
+    if (numArr.length >= 9){
+        bigNumArea.innerText = bigNumArea.innerText.slice(1);
+    };
+    if (bigNumArea.innerText != 0) {
+        bigNumArea.innerText += event.target.innerText;
+    } else {
+        bigNumArea.innerText = event.target.innerText;
+    };
+
+};
 // ASSINGNING THE FUNCTIONS
 clearBtn.addEventListener('click', clear)
+for (const btn of numBtns) {
+    btn.addEventListener('click', display);
+};
+for (const btn of operatorBtns) {
+    btn.addEventListener('click', display)
+}
