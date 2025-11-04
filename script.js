ef162df72809336a452df1550c521d674ae9ee13
Tulio <221194504+tulin404@users.noreply.github.com>
2025-11-03 22:28:02 -0300

    Finished the calculator, proud of myself

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const smallNumArea = document.getElementById('small-num-area');
 const numBtns = document.getElementsByClassName('num-btn');
 const operatorBtns = document.getElementsByClassName('operator-btn');
 const clearBtn = document.getElementById('clear');
+const deleteBtn = document.getElementById('delete');
+const equalBtn = document.getElementById('equal');
 
 // CREATING THE BASIC FUNCTIONS
 function clear() {
@@ -22,13 +24,36 @@ function display(event) {
     } else {
         bigNumArea.innerText = event.target.innerText;
     };
-
 };
+
+function equal() {
+    let expression = bigNumArea.innerText;
+    smallNumArea.innerText = expression;
+    if (expression.includes('x')) {
+        expression = expression.replace('x', '*')
+    } else if (expression.includes('÷')) {
+        expression = expression.replace('÷', '/')
+    }
+    bigNumArea.innerText = math.evaluate(expression);
+};
+
+function del() {
+    bigNumArea.innerText = bigNumArea.innerText.slice(0, bigNumArea.innerText.length - 1)
+    if (bigNumArea.innerText.length < 1) {
+        bigNumArea.innerText = 0;
+    };
+};
+
 // ASSINGNING THE FUNCTIONS
-clearBtn.addEventListener('click', clear)
+clearBtn.addEventListener('click', clear);
+
 for (const btn of numBtns) {
     btn.addEventListener('click', display);
 };
+
 for (const btn of operatorBtns) {
     btn.addEventListener('click', display)
-}
+};
+
+equalBtn.addEventListener('click', equal);
+deleteBtn.addEventListener('click', del);
